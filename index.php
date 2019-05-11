0e8b21e0a81b6a02cb35ae6af6bf72a345f5ce00
Phil Roggenbuck <ceo@roggstar.eu>
2019-05-11 15:31:01 +0200

    Test

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+	  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+	  <link rel="icon" href="/favicon.ico" type="image/x-icon">
+	  <style>
+		  body{
+			  text-align:center;
+			  background-color:#7f08e0;
+		  }
+	  </style>
+    <title>PhishingRipper</title>
+  </head>
+  <body>
+	
+  </body>
+</html>
+
+<?php
+    //exec("phishingripper.py");
+?>
