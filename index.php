9a42a83446267d1b27f7f56178124bddb2bee0c8
Phil Roggenbuck <ceo@roggstar.eu>
2019-05-11 15:31:09 +0200

    Test

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
-  <head>
-	  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+	<head>
+		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 	  <link rel="icon" href="/favicon.ico" type="image/x-icon">
 	  <style>
 		  body{
@@ -11,7 +11,14 @@
     <title>PhishingRipper</title>
   </head>
   <body>
-	
+		<form action="upload.php?submit=1" method="post" enctype="multipart/form-data">
+			<input type="text" name="url" hint="https://blablabtgergerg.com/post.php"/>
+			<select name="typ" size="1"><option value="" selected disabled hidden>Request Type</option>
+				<option>_POST</option>
+				<option>_GET</option>
+		</select>
+
+		</form>
   </body>
 </html>
 
